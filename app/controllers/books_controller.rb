f119505a18b05fe84219f016820a9fcfdba24c97
ShoNakanishi <165697131+sho-R4i@users.noreply.github.com>
2024-04-21 05:33:33 +0000

    [Update]show,edit,update済(destroy未)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,23 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path(@book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(@book)
   end
 
 
