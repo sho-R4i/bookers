af378c3277190d83cc1bbbe740c92806b1828042
ShoNakanishi <165697131+sho-R4i@users.noreply.github.com>
2024-04-22 13:59:35 +0000

    [Update]フラッシュメッセージ,バリデーションの実装(create未)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,11 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully created."
+      flash[:notice] = 'Book was successfully created.'
       redirect_to book_path(@book.id)
     else
       render :new
@@ -27,14 +27,22 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to books_path(@book)
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to books_path(@book)
+    else
+      render :show
+    end
   end
-  
+
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path(@book)
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book was successfully destroyed"
+      redirect_to books_path
+    else
+      render :show
+    end 
   end
 
 
