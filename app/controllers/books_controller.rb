30c39c810ff0288dcf073f304af704bf369effa5
ShoNakanishi <165697131+sho-R4i@users.noreply.github.com>
2024-04-26 02:44:09 +0000

    [Update]更新が失敗した時のエラーメッセージを表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,9 +30,9 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = 'Book was successfully updated.'
-      redirect_to books_path(@book)
+      redirect_to book_path(@book)
     else
-      render :show
+      render :edit
     end
   end
 
